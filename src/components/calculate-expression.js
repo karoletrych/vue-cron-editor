5bf13dda99f97d94c38b02c52e0b6a783a00373e
Karol Etrych <karol.etrych@gmail.com>
2019-11-10 01:39:10 +0100

    add hourly and daily to calculateexpression

--- src/components/calculate-expression.js
+++ src/components/calculate-expression.js
@@ -1,14 +1,18 @@
 export const calculateExpression = (event) => {
     if(event.type === "minutes")
     {
-        return event.minutes;
+        return `0/${event.minutes} * 1/1 * ?`;
     }
     else if (event.type === "hourly")
     {
-        return event.hourly;
+        return `${event.hours} 0/${event.minutes} 1/1 * ?`;
     }
     else if (event.type === "daily")
     {
-        return event.daily;
+        return `${event.minutes} ${event.hours} 1/${dayInterval} * ?`
     }
+}
+
+export const tabFromExpression = (expression) => {
+
 }
