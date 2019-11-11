fa2ad2bb128854d5f4fbc066e724988bae8e2dd7
Karol Etrych <karol.etrych@gmail.com>
2019-11-11 01:50:46 +0100

    use AbstractSelect in Daily

--- src/components/Daily.vue
+++ src/components/Daily.vue
@@ -1,47 +1,51 @@
 <template>
-    <div class="inline">
-        Every
-        <v-select
-          dense
-          :items="Array(31).fill(0).map(Number.call, Number)"
-          @change="notifyParent"
-          />
-        day(s) at
-        <v-select
-          dense
-          :items="Array(24).fill(0).map(Number.call, Number)"
-          @change="notifyParent"
-          />
-        <v-select
-          dense
-          :items="Array(60).fill(0).map(Number.call, Number)"
-          @change="notifyParent"
-          />
-    </div>
+  <div class="inline">
+    Every
+    <AbstractSelect :items="Array(31).fill(0).map(Number.call, Number)" v-model="dayInterval" />day(s) at
+    <AbstractSelect :items="Array(24).fill(0).map(Number.call, Number)" v-model="hours" />
+    <AbstractSelect :items="Array(60).fill(0).map(Number.call, Number)" v-model="minutes" />
+  </div>
 </template>
 
 <script>
+import AbstractSelect from "./AbstractSelect";
 
 export default {
-  name: 'Daily',
+  name: "Daily",
+  components: { AbstractSelect },
   inject: ["eventBus"],
-  computed: {
-
-  },
   data: () => ({
-
+    dayInterval: null,
+    minutes: null,
+    hours: null
   }),
-  methods:{
-    notifyParent(newValue){
-      const event = {type: "daily", minutes: newValue};
-      this.eventBus.$emit("expressionChanged", event)
+  watch: {
+    dayInterval() {
+      this.notifyParent();
+    },
+    hours() {
+      this.notifyParent();
+    },
+    minutes() {
+      this.notifyParent();
+    }
+  },
+  methods: {
+    notifyParent() {
+      const event = {
+        type: "daily",
+        hours: this.hours,
+        minutes: this.minutes,
+        dayInterval: this.dayInterval
+      };
+      this.eventBus.$emit("expressionChanged", event);
     }
   }
-}
+};
 </script>
 
 <style>
-.inline{
-    display: inline-flex
+.inline {
+  display: inline-flex;
 }
 </style>
