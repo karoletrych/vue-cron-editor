b589f9c3cc233f14d2f67f417c32d98df9af82f8
Karol Etrych <karol.etrych@gmail.com>
2019-11-24 21:13:34 +0100

    add datagrid sample

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
 <template>
     <v-app>
+        <v-data-table :headers="headers" :items-per-page="5" :items="expressions">
+            <template #top>
+                <v-toolbar flat color="white">
+                    <v-toolbar-title>CRUD expressions sample</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>
+                    <v-spacer></v-spacer>
+                    <v-dialog v-model="dialog" max-width="800px">
+                        <template v-slot:activator="{ on }">
+                            <v-btn color="primary" dark class="mb-2" v-on="on">New Expression</v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-title>
+                                <span class="headline">New Expression</span>
+                            </v-card-title>
+
+                            <v-card-text>
+                                <v-container>
+                                    <vue-cron-editor v-model="editedItem.expression">
+                                    </vue-cron-editor>
+                                </v-container>
+                            </v-card-text>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-toolbar>
+            </template>
+            <template #item.action="{ item }">
+                <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
+                <v-icon small @click="deleteItem(item)">delete</v-icon>
+            </template>
+        </v-data-table>
         <VueCronEditor v-model="cronExpression"></VueCronEditor>
         <v-text-field v-model="cronExpression" />
     </v-app>
@@ -13,9 +49,42 @@ export default {
     components: {
         VueCronEditor
     },
-    mounted() {},
+    methods: {
+        editItem(item) {
+            this.editedIndex = this.expressions.indexOf(item);
+            this.editedItem = item;
+            this.dialog = true;
+        },
+        deleteItem(item) {
+            const index = this.expressions.indexOf(item);
+            confirm("Are you sure you want to delete this item?") &&
+                this.expressions.splice(index, 1);
+        },
+        close() {
+            this.dialog = false;
+            setTimeout(() => {
+                this.editedIndex = -1;
+            }, 300);
+        },
+        save() {
+            if (this.editedIndex > -1) {
+                this.expressions[this.editedIndex] = this.editedItem;
+            } else {
+                this.expressions.push(this.editedItem);
+            }
+            this.close();
+        }
+    },
     data: () => ({
-        cronExpression: "4 4 * * 0,2,3,5"
+        headers: [
+            { text: "Id", value: "id" },
+            { text: "Expression", value: "expression" },
+            { text: "Actions", value: "action", sortable: false }
+        ],
+        expressions: [{ id: 0, expression: "4 4 * * 0,2,3,5" }],
+        editedItem: {expression: ""},
+        dialog: false,
+        editedIndex: null
     })
 };
 </script>
