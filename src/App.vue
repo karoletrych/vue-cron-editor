7da3ae01c774fc7db349a3e505f67db263346316
Karol Etrych <karol.etrych@gmail.com>
2019-12-29 20:53:00 +0100

    add simple component to samples

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,64 @@
 <template>
     <v-app>
-<!-- 
-        <VueCronEditor v-model="cronExpression"></VueCronEditor>
-        CRON expression: {{cronExpression"}} -->
+        <v-container>
+            <section>
+                <VueCronEditor v-model="sample1CronExpression"></VueCronEditor>
+                CRON expression: {{sample1CronExpression}}
+            </section>
 
-        <v-data-table :headers="headers" :items-per-page="5" :items="expressions">
-            <template #top>
-                <v-toolbar flat color="white">
-                    <v-toolbar-title>CRUD expressions sample</v-toolbar-title>
-                    <v-divider class="mx-4" inset vertical></v-divider>
-                    <v-spacer></v-spacer>
-                    <v-dialog v-model="dialog" max-width="800px">
-                        <template v-slot:activator="{ on }">
-                            <v-btn color="primary" dark class="mb-2" v-on="on">New Expression</v-btn>
-                        </template>
-                        <v-card>
-                            <v-card-title>
-                                <span class="headline">Adding a CRON expression</span>
-                            </v-card-title>
+            <section>
+                <v-data-table :headers="headers" :items-per-page="5" :items="expressions">
+                    <template #top>
+                        <v-toolbar flat color="white">
+                            <v-toolbar-title>CRUD expressions sample</v-toolbar-title>
+                            <v-divider class="mx-4" inset vertical></v-divider>
+                            <v-spacer></v-spacer>
+                            <v-dialog v-model="dialog" max-width="800px">
+                                <template v-slot:activator="{ on }">
+                                    <v-btn
+                                        color="primary"
+                                        dark
+                                        class="mb-2"
+                                        v-on="on"
+                                    >New Expression</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="headline">Adding a CRON expression</span>
+                                    </v-card-title>
 
-                            <v-card-text>
-                                <v-container>
-                                    <vue-cron-editor 
-                                    v-model="editedItem.expression"
-                                    locale="test"
-                                    :custom-locales="customLocales"
-                                    >
-                                    </vue-cron-editor>
+                                    <v-card-text>
+                                        <v-container>
+                                            <vue-cron-editor
+                                                v-model="editedItem.expression"
+                                                locale="test"
+                                                :custom-locales="customLocales"
+                                            ></vue-cron-editor>
+                                            CRON expression: {{editedItem.expression}}
+                                        </v-container>
+                                    </v-card-text>
 
-                                    CRON expression: {{editedItem.expression}}
-                                </v-container>
-                            </v-card-text>
-
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
-                </v-toolbar>
-            </template>
-            <template #item.action="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
-                <v-icon small @click="deleteItem(item)">delete</v-icon>
-            </template>
-        </v-data-table>
-
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-toolbar>
+                    </template>
+                    <template #item.action="{ item }">
+                        <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
+                        <v-icon small @click="deleteItem(item)">delete</v-icon>
+                    </template>
+                </v-data-table>
+            </section>
+        </v-container>
     </v-app>
 </template>
 
 <script>
 import VueCronEditor from "./components/VueCronEditor";
-
 
 export default {
     name: "App",
@@ -74,12 +80,15 @@ export default {
             this.dialog = false;
             setTimeout(() => {
                 this.editedIndex = -1;
-                this.editedItem = {}
+                this.editedItem = {};
             }, 300);
         },
         save() {
             if (this.editedIndex > -1) {
-                Object.assign(this.expressions[this.editedIndex], this.editedItem);
+                Object.assign(
+                    this.expressions[this.editedIndex],
+                    this.editedItem
+                );
             } else {
                 const newIndex = this.expressions.push(this.editedItem);
                 this.expressions[newIndex - 1].id = newIndex;
@@ -88,7 +97,8 @@ export default {
         }
     },
     data: () => ({
-        customLocales:{
+        sample1CronExpression: "4 4 * * 0,2,3,5",
+        customLocales: {
             test: {
                 every: "Every",
                 minutes: "minute(s)",
@@ -118,7 +128,7 @@ export default {
         expressions: [{ expression: "4 4 * * 0,2,3,5", id: 0 }],
         editedItem: {},
         dialog: false,
-        editedIndex: -1,
+        editedIndex: -1
     })
 };
 </script>
